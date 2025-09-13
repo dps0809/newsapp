a006a7483d551f7d9e75b57d54e6f7a8d1736263
dps0809 <divyaprakash.hp1973@gmail.com>
2025-09-13 20:11:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default class App extends Component {
             <Route path="/search" element={<NewsItems ref={this.newsRef} key="general" pageSize={12} country="us" category="searched" />} />
             <Route path="/sports" element={<NewsItems ref={this.newsRef} key="sports" pageSize={12} country="us" category="sports" />} />
             <Route path="/business" element={<NewsItems ref={this.newsRef} key="business" pageSize={12} country="us" category="business" />} />
+            <Route path="/entertainment" element={<NewsItems ref={this.newsRef} key="entertainment" pageSize={12} country="us" category="entertainment" />} />
             <Route path="/health" element={<NewsItems ref={this.newsRef} key="health" pageSize={12} country="us" category="health" />} />
             <Route path="/science" element={<NewsItems ref={this.newsRef} key="science" pageSize={12} country="us" category="science" />} />
             <Route path="/technology" element={<NewsItems ref={this.newsRef} key="technology" pageSize={12} country="us" category="technology" />} />
